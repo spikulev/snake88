d2e8acf66c50d0bd60920b7bc40f88f902716ec2
spikulev <serpik1988@yandex.ru>
2019-07-27 20:38:14 +0500

    Наследование

---
+++ snake88/snake88/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake88
+{
+    class Employee : Human
+    {
+        public int salary;
+    }
+}
