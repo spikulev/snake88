d6a32d536d6aee69427285de89f5175a281553b8
spikulev <serpik1988@yandex.ru>
2019-07-28 15:40:39 +0500

    Змейка получила управление с клавиатуры

--- snake88/snake88/Program.cs
+++ snake88/snake88/Program.cs
@@ -21,28 +21,22 @@ namespace snake88
             downline.Draw();
             leftline.Draw();
             rightline.Draw();
-
-            Point p = new Point(2, 12, '*');
-            Snake snake = new Snake(p, 4, Direction.UP);
+            
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
 
-            Console.ReadLine();
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                Thread.Sleep(100);
+                snake.Move();
+            }
         }
     }
 }
--- snake88/snake88/Snake.cs
+++ snake88/snake88/Snake.cs
@@ -40,5 +40,17 @@ namespace snake88
             nextPoint.Move(1, direction);
             return nextPoint;
         }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+        }
     }
 }
